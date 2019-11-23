cfb1a444e3391712e5b4f393c8f71b4965272128
nguyentduc <ntduc.121@gmail.com>
2019-11-23 14:02:59 +0200

    add stub-fuction, function prototypes and example test cases

---
+++ linked_list.h
@@ -0,0 +1,13 @@
+
+struct linked_list{
+    char *data;
+    int index; /* position of the element in the list, normally not used with Linked Lists, just for exercise */
+    struct linked_list *next;
+};
+typedef struct linked_list linked_list;
+
+int add_to_list(linked_list *ll, char *s);
+int display_item(linked_list *ll);
+int display_list(linked_list *ll);
+linked_list * search_from_list(linked_list *ll, char *s);
+int delete_from_list(linked_list *ll, int index);
